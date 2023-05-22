055fc8dc8c2a18d79be374648b0f8a4b12cb6385
Yura <113457886+Yura-Platonov@users.noreply.github.com>
2023-05-22 23:54:34 +0300

    made all tasks

---
+++ src/js/02-timer.js
@@ -0,0 +1,86 @@
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+
+const startButton = document.querySelector('[data-start]');
+const datetimePicker = document.querySelector('#datetime-picker');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+let countdownInterval;
+
+function startCountdown() {
+  const targetDate = new Date(datetimePicker.value).getTime();
+
+  countdownInterval = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    if (distance < 0) {
+      clearInterval(countdownInterval);
+      resetTimer();
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(distance);
+
+    daysElement.textContent = addLeadingZero(days);
+    hoursElement.textContent = addLeadingZero(hours);
+    minutesElement.textContent = addLeadingZero(minutes);
+    secondsElement.textContent = addLeadingZero(seconds);
+  }, 1000);
+}
+
+function resetTimer() {
+  daysElement.textContent = '00';
+  hoursElement.textContent = '00';
+  minutesElement.textContent = '00';
+  secondsElement.textContent = '00';
+  datetimePicker.value = '';
+  startButton.disabled = true;
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+  
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate.getTime() < Date.now()) {
+      window.alert('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr("#datetime-picker", options);
+
+startButton.addEventListener('click', startCountdown);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+const messagesContainer = document.querySelector('#messages');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  const initialDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const delay = initialDelay + i * step;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        const message = document.createElement('div');
+        message.classList.add('message', 'fulfilled');
+        message.textContent = `✅ Fulfilled promise ${position} in ${delay}ms`;
+        messagesContainer.appendChild(message);
+      })
+      .catch(({ position, delay }) => {
+        const message = document.createElement('div');
+        message.classList.add('message', 'rejected');
+        message.textContent = `❌ Rejected promise ${position} in ${delay}ms`;
+        messagesContainer.appendChild(message);
+      });
+  }
+});
